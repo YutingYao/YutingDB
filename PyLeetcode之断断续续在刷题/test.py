dc79744a476368e84db9f1d6290b5e38ca4c4aa2
YutingYao <30456879+YutingYao@users.noreply.github.com>
2022-04-26 17:48:10 +0800

    update

--- PyLeetcode之断断续续在刷题/test.py
+++ PyLeetcode之断断续续在刷题/test.py
@@ -1,15 +1,10 @@
 class Solution:
-    def verifyPostorder(self, postorder: List[int]) -> bool:
-        def isBetween(postorder: List[int], ma: int, mi: int):
-            if postorder: 
-                val = postorder[-1]
-                if mi < val < ma:
-                    postorder.pop() # 根
-                    isBetween(postorder, ma, val) # 右
-                    isBetween(postorder, val, mi) # 左
-
-        isBetween(postorder, sys.maxsize, -sys.maxsize)
-        '''
-        如果是 postorder 的话，返回结果应该是空
-        '''
-        return not postorder 
+    def largestNumber(self, nums: List[int]) -> str:
+        # 按照字典序由大到小排序
+        nums = sorted([str(x) for x in nums], reverse = True)
+        for stt in range(len(nums) - 1):
+            for end in range(stt, len(nums)):
+        # [3,30,34,5,9] 的 3,30 不能按照字典序排序，需要交换位置
+                if str(nums[stt]) + str(nums[end]) < str(nums[end]) + str(nums[stt]):
+                    nums[stt], nums[end] = nums[end], nums[stt]
+        return str(int(''.join(nums)))
